81cdf28edac60c658ce756f6258750a619221083
242560 Wilczyński Maciej <242560@edu.p.lodz.pl>
2023-04-05 01:12:28 +0200

    maven ogarniety

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import org.deeplearning4j.nn.api.NeuralNetwork;
+import org.deeplearning4j.nn.graph.ComputationGraph;
+
+
 public class Main {
     public static void main(String[] args) {
+
+
         System.out.println("Hello world!");
     }
 }
