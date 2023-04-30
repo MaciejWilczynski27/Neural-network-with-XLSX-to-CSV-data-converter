923acd7f24a02e34fcc673f173719e6be50aa92e
242560 Wilczyński Maciej <242560@edu.p.lodz.pl>
2023-05-01 00:32:31 +0200

    minor improvements

--- src/main/java/org/example/DataForNeuralNetwork.java
+++ src/main/java/org/example/DataForNeuralNetwork.java
@@ -12,21 +12,12 @@ import java.io.IOException;
 import java.util.List;
 
 public class DataForNeuralNetwork {
-    public static void prepareData(String sourceDirectory, String targetDirectory, String fileName, int firstIndex, int lastIndex,boolean isTraining) throws Exception {
-        String dataType;
+    public static void prepareTrainingData(String sourceDirectory, String targetDirectory, String fileName, int firstIndex, int lastIndex) throws Exception {
+        String dataType = "_training";;
         for(int i = firstIndex; i < lastIndex + 1; i++) {
             StringBuilder stringBuilder = new StringBuilder(sourceDirectory);
             Workbook workbook = new Workbook(stringBuilder.append(fileName).append(i).append(".xlsx").toString());
-            Worksheet worksheet = workbook.getWorksheets().get(0);
-            worksheet.getCells().deleteRows(0, 1, true);
-            if(isTraining) {
-                worksheet.getCells().deleteColumns(0, 23, true);
-                worksheet.getCells().deleteColumns(2, 7, true);
-                worksheet.getCells().deleteColumns(4, 1, true);
-                dataType = "_training";
-            } else {
-                dataType = "_dynamic";
-            }
+            prepareColumns(workbook);
             stringBuilder = new StringBuilder(targetDirectory).append(fileName).append(i).append(dataType).append(".csv");
             String pathToConverted = stringBuilder.toString();
             workbook.save(pathToConverted);
@@ -35,6 +26,27 @@ public class DataForNeuralNetwork {
             saveCsvFile(rows, pathToConverted);
         }
     }
+    public static void prepareDynamicData(String sourceDirectory, String targetDirectory,String fileName) throws Exception {
+        StringBuilder stringBuilder = new StringBuilder(sourceDirectory);
+        Workbook workbook = new Workbook(stringBuilder.append(fileName).append(".xlsx").toString());
+        prepareColumns(workbook);
+        String dataType = "_dynamic";
+        stringBuilder = new StringBuilder(targetDirectory).append(fileName).append(dataType).append(".csv");
+        String pathToConverted = stringBuilder.toString();
+        workbook.save(pathToConverted);
+        List<String[]> rows = readCsvFile(pathToConverted);
+        rows.remove(rows.size() - 1);
+        saveCsvFile(rows, pathToConverted);
+    }
+
+    private static void prepareColumns(Workbook workbook) {
+        Worksheet worksheet = workbook.getWorksheets().get(0);
+        worksheet.getCells().deleteRows(0, 1, true);
+        worksheet.getCells().deleteColumns(0, 23, true);
+        worksheet.getCells().deleteColumns(2, 7, true);
+        worksheet.getCells().deleteColumns(4, 1, true);
+    }
+
     private static List<String[]> readCsvFile(String filePath) throws IOException {
         CSVReader csvReader = new CSVReader(new FileReader(filePath));
         List<String[]> rows = csvReader.readAll();
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        DataForNeuralNetwork.prepareData("dataXLSX/F8/","dataCSV/F8/","f8_stat_",1,225,true);
-        DataForNeuralNetwork.prepareData("dataXLSX/F10/","dataCSV/F10/","f10_stat_",1,225,true);
+      //  DataForNeuralNetwork.prepareData("dataXLSX/F8/","dataCSV/F8/","f8_stat_",1,225);
+       // DataForNeuralNetwork.prepareData("dataXLSX/F10/","dataCSV/F10/","f10_stat_",1,225,true);
+        //DataForNeuralNetwork.prepareDynamicData("dataXLSX/F8/","dataCSV/F8/","f8_1p");
 
     }
 }
